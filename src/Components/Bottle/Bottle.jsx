15bc7a7e07482ebde7b147a888c76108a41f3470
junayedgit <ahmedjonayed802@gmail.com>
2025-08-19 15:01:33 +0600

    add

--- src/Components/Bottle/Bottle.jsx
+++ src/Components/Bottle/Bottle.jsx
@@ -1,11 +1,12 @@
 import "./Bottle.css";
-const Bottle = ({ bottle }) => {
+const Bottle = ({ bottle, handleAddToCart }) => {
   const { name, img, price } = bottle;
   return (
     <div className="bottle">
       <h2>Bottle : {name}</h2>
       <img src={img} alt="" />
       <p>price: ${price}</p>
+      <button onClick={() => handleAddToCart(bottle)}>Purches</button>
     </div>
   );
 };
